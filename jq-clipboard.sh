895ac29cfd53c794b44a5051e9996cf957382945
Luke Randall <luke.randall@gmail.com>
2021-04-29 13:04:30 +0100

    Add jq clipboard

---
+++ jq-clipboard.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.author Luke Randall
+# @raycast.authorURL https://github.com/lukerandall
+# @raycast.title Clipboard → jq
+# @raycast.mode fullOutput
+# @raycast.packageName Developer Utilities
+
+# Optional parameters:
+# @raycast.icon images/json.png
+
+# Documentation:
+# @raycast.description Displays pretty printed JSON from clipboard.
+
+# @raycast.argument1 { "type": "text", "placeholder": "Query", "optional": false }
+
+pbpaste | jq "$1"
