697ba3a5087cb797b2fd44a650452f16a6242615
Luke Randall <luke.randall@gmail.com>
2021-04-30 09:39:04 +0100

    Add protonmail script

---
+++ open-protonmail.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open ProtonMail
+# @raycast.mode silent
+# @raycast.packageName Mail
+#
+# Optional parameters:
+# @raycast.icon 📧
+#
+# Documentation:
+# @raycast.description Open ProtonMail
+# @raycast.author Luke Randall
+# @raycast.authorURL https://github.com/lukerandall
+
+open "http://beta.protonmail.com/u/0/inbox"
